8b99a937049e86fa789a5f60eefe4a4abd80ea8e
maurya512 <patelmaurya0512@gmail.com>
2021-09-13 09:32:20 -0400

    imported single post comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // * import dependencies
 import Navbar from "./components/Navbar/Navbar";
-import Home from "./pages/Home/Home";
+// import Home from "./pages/Home/Home";
+import SinglePost from "./pages/SinglePost/SinglePost";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <SinglePost />
     </>
   );
 }
