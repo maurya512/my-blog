3102c4e2a1f3deec3da3712dde9d10f258946524
maurya512 <patelmaurya0512@gmail.com>
2021-09-14 09:52:09 -0400

    imported the write component to main page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import Navbar from "./components/Navbar/Navbar";
 // import Home from "./pages/Home/Home";
 import SinglePost from "./pages/SinglePost/SinglePost";
+import Write from "./pages/Write/Write";
 
 function App() {
   return (
     <>
       <Navbar />
-      <SinglePost />
+      <Write />
     </>
   );
 }
