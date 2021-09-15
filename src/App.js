8c8e8144fabda19a85c30f40f963896b52b69a80
maurya512 <patelmaurya0512@gmail.com>
2021-09-15 09:22:58 -0400

    imported the settings component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 // * import dependencies
 import Navbar from "./components/Navbar/Navbar";
+import Settings from "./pages/Settings/Settings";
 // import Home from "./pages/Home/Home";
-import SinglePost from "./pages/SinglePost/SinglePost";
-import Write from "./pages/Write/Write";
+// import SinglePost from "./pages/SinglePost/SinglePost";
+// import Write from "./pages/Write/Write";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Write />
+      {/* <Write /> */}
+      <Settings />
     </>
   );
 }
