15f197235f0a9552d1daea9fba5a6457b0667cd8
maurya512 <patelmaurya0512@gmail.com>
2021-09-26 19:16:17 -0400

    created routes for categories

---
+++ api/routes/categories.js
@@ -0,0 +1,28 @@
+// ! import router dependency 
+const router = require("express").Router();
+
+// ! import Category model 
+const Category = require("../models/Category");
+// ! create a category
+router.post("/", async (req, res) => {
+    const newCategory = new Category(req.body);
+    try {
+        const savedCategory = await newCategory.save();
+        res.status(200).json(savedCategory);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// ! get all categories
+router.get("/", async (req, res) => {
+    try {
+        const categories = await Category.find();
+        res.status(200).json(categories);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// ! exporting default
+module.exports = router;
