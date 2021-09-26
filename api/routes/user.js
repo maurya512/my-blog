6e7961722646f8279907f0f2bc329661eaf5b261
maurya512 <patelmaurya0512@gmail.com>
2021-09-25 22:30:36 -0400

    Added a route to delete the user

--- api/routes/user.js
+++ api/routes/user.js
@@ -7,6 +7,9 @@ const User = require("../models/User");
 // ! import bcrypt
 const bcrypt = require("bcrypt");
 
+// ! import posts 
+const Post = require("../models/Post");
+
 // ! Routes to update the user
 // * updating the user based on their user id
 router.put("/:id", async (req, res) => {
@@ -17,9 +20,11 @@ router.put("/:id", async (req, res) => {
             req.body.password = await bcrypt.hash(req.body.password, salt);
         }
         try {
+            // ! updates the user with new information that was changed 
             const updatedUser = await User.findByIdAndUpdate(req.params.id, {
                 $set: req.body,
-            });
+                // ! displays the updated info for user 
+            }, { new: true });
             res.status(200).json(updatedUser)
         } catch (error) {
             // ! 500 status code is for errors
@@ -30,6 +35,31 @@ router.put("/:id", async (req, res) => {
         res.status(401).json("User can only update their own account");
     }
 })
+
+// ! Delete the user 
+// * deleting the user based on the id provided in the url
+router.delete("/:id", async (req, res) => {
+    // ! verifying whether the user is in the database
+    if (req.body.userId === req.params.id) {
+        // ! get and delete all posts of user
+        try {
+            const user = await User.findById(req.params.id);
+            try {
+                // ! if a user is found delete the user and display a message to the user
+                await Post.deleteMany({ username: user.username })
+                await User.findByIdAndDelete(req.params.id)
+                res.status(200).json("User has been deleted");
+            } catch (err) {
+                res.status(500).json(err)
+            }
+        } catch (err) {
+            res.status(404).json("User not found");
+        }
+    }
+    else {
+        res.status(401).json("You can only delete your account");
+    }
+});
 
 // ! exporting module
 module.exports = router;
